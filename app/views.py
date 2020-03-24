0d6ab757bcbf86ba3d5d7bdd1af2ab0c5dff15e0
xncbf <xncbf12@gmail.com>
2020-03-25 00:17:28 +0900

    Add AJAX

--- app/views.py
+++ app/views.py
@@ -1,6 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 from django.views.generic import TemplateView
 
 # Create your views here.
 class IndexView(TemplateView):
     template_name = 'index.html'
+
+    def post(self, request, *args, **kwargs):
+        return HttpResponse(status=201)
--- resize/urls.py
+++ resize/urls.py
@@ -19,5 +19,5 @@ from app.views import IndexView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', IndexView.as_view())
+    path('', IndexView.as_view(), name='index')
 ]
